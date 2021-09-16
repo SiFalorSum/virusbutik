f536add1d1494740afa52ca74596456873b9b145
EmilB <emilbelin@live.se>
2021-09-16 12:02:20 +0200

    Dan, Emil. Added javascript for menu toggling.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,14 @@
 /* DARKMODE FUNCTIONS AND LISTENERS */
 
 let body = document.getElementById('body');
+let menuButton = document.getElementById('menuButton');
+let menuBar = document.getElementById('menuBarID');
+let menuVisible = true;
+
+menuButton.addEventListener("click", e=> toggleMenu());
+document.getElementById('darkmode-toggle').addEventListener('click', e => toggleDarkMode());
+document.addEventListener('DOMContentLoaded', e => darkModeListener());
+window.addEventListener('storage', e => darkModeListener());
 
 function toggleDarkMode() {
     window.localStorage.setItem('darkmode', body.classList.toggle('darkmode')); //toggle returns true or false
@@ -11,6 +19,16 @@ function darkModeListener() {
     body.classList.toggle('darkmode', isDarkMode); //equivalent to 'add' if true, 'remove' if false
 }
 
-document.getElementById('darkmode-toggle').addEventListener('click', e => toggleDarkMode());
-document.addEventListener('DOMContentLoaded', e => darkModeListener());
-window.addEventListener('storage', e => darkModeListener());
+function toggleMenu()
+{
+    if(menuVisible)
+    {
+        menuBar.style.display = "none";
+        menuVisible = !menuVisible;
+    }
+    else
+    {
+        menuBar.style.display = "block";
+        menuVisible = !menuVisible;
+    }
+}
