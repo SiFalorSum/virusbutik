421a6ec78b99cd5eeb232fcb9eaf2f3987e1327a
Jaxtar <39218584+Jaxtar@users.noreply.github.com>
2021-09-20 20:30:38 +0200

    Fix JSON load

--- scripts/products.js
+++ scripts/products.js
@@ -2,6 +2,35 @@
 
 let cart;
 
+let productsString = `{
+    "products" : [
+        {
+            "id" : "1",
+            "name" : "Hepatit C",
+            "summary" : "Löksås ipsum smultron sjö",
+            "description" : "Löksås ipsum smultron sjö icke både kunde tiden jäst strand åker, faktor tid på dimmhöljd sin sjö äng tre. Från redan räv i brunsås bäckasiner är stig mjuka bra söka, dimmhöljd träutensilierna bland har från söka plats både nu sista det, dag som stig denna inom söka färdväg se bra. Har tidigare groda dunge enligt färdväg hwila kan häst nu rot redan sorgliga, dag björnbär trevnadens sig verkligen helt på jäst vidsträckt varit.",
+            "price" : 295,
+            "imageURL" : "../images/cookie-the-pom-gySMaocSdqs-unsplash.jpg"
+        },
+        {
+            "id" : "2",
+            "name" : "Hårddiskherpes",
+            "summary" : "Vidsträckt bäckasiner kom lax brunsås",
+            "description" : "Vidsträckt bäckasiner kom lax brunsås samma göras hav bäckasiner, tre olika ska både helt annat tidigare söka, jäst verkligen kanske sista vad både och. Bra stora dag nya sig annan räv ingalunda, vidsträckt tidigare i sjö sitt icke omfångsrik, och flera hav bäckasiner har år. Samma del häst varit dimmhöljd plats hela att för, gamla bäckasiner det varit ska verkligen år, genom tid se sjö som upprätthållande faktor.",
+            "price" : 395,
+            "imageURL" : "../images/cookie-the-pom-gySMaocSdqs-unsplash.jpg"
+        },
+        {
+            "id" : "3",
+            "name" : "Webbklamydia",
+            "summary" : "Det dock annat dimma hans nya göras",
+            "description" : "Dimma jäst gör rännil tre som händer vad bland, som nya samtidigt är och blivit tid, dag brunsås se därmed tidigare mot blivit. Jäst när se både har ser händer när, bäckasiner kunde sitt och göras sjö. Det dock annat dimma hans nya göras händer där det enligt flera, inom när vidsträckt annat är räv plats kunde annat.",
+            "price" : 199,
+            "imageURL" : "../images/cookie-the-pom-gySMaocSdqs-unsplash.jpg"
+        }
+    ]
+}`;
+
 function loadCartFromStorage() {
     cart = JSON.parse(window.localStorage.getItem('cart'));
 }
@@ -22,20 +51,6 @@ function addToCart(productID) {
 
 document.addEventListener('DOMContentLoaded', e => loadCartFromStorage());
 window.addEventListener('storage', e => loadCartFromStorage());
-
-async function fetchJSON(path, responseProcessorFunction) {
-    try {
-        let response = await fetch(path);
-        if (!response.ok) 
-            throw new Error(`Unable to load data from ${response.url}` 
-            + `\nStatus: ${response.status} - "${response.statusText}"`);
-        let products = await response.json();
-        responseProcessorFunction(products);
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
 
 function setAddToCartButtonInnerText(addToCartBtn, productID) {
     if (cartHasProduct(productID)) {
@@ -78,4 +93,4 @@ function renderProductsPage(productsJSON) {
     });
 }
 
-fetchJSON('../json/products.json', renderProductsPage);
+renderProductsPage(JSON.parse(productsString));
