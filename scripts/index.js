4cfad30e782b1b95c06b31acbb442ff641b32608
BitXplorer <70508247+BitXplorer@users.noreply.github.com>
2021-09-20 17:09:35 +0200

    trying to add checkout page.

--- scripts/index.js
+++ scripts/index.js
@@ -98,3 +98,23 @@ function toggleMenu()
 }
 
 menuButton.addEventListener("click", e=> toggleMenu());
+
+
+(function () {
+  'use strict'
+
+  var forms = document.querySelectorAll('.needs-validation')
+
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
